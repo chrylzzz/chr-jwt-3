1ff5c424ee329e12f2dcfdb9bb1daf899f0670b7
chryl <cyl5yy412@gmail.com>
2020-03-20 21:23:46 +0800

    init

--- src/main/java/com/chryl/utils/JwtUtil.java
+++ src/main/java/com/chryl/utils/JwtUtil.java
@@ -1,17 +1,11 @@
 package com.chryl.utils;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.Claim;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.chryl.bean.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
-import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
